f9b4abeb3cfe85675306819cf5d9f1f2759e469a
Betsy Carina Traran <betsy.carina.traran@nav.no>
2021-04-22 12:59:55 +0200

    All exercises

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -14,16 +14,48 @@ function UsernameForm({onSubmitUsername}) {
   // 💰 For example: event.target.elements[0].value
   // 🐨 Call `onSubmitUsername` with the value of the input
 
+  // const [errorMessage, setErrorMessage] = React.useState(null)
+
+  const [inputLowercase, setInputLowercase] = React.useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    // const value = event.target.elements.user.value
+    onSubmitUsername(inputLowercase)
+  }
+
+  // const handleChange = event => {
+  //   const value = event.target.value
+  //   const isValid = value === value.toLowerCase()
+  //   setErrorMessage(
+  //     isValid ? null : 'Brukernavnet kan ikke inneholde store bokstaver!',
+  //   )
+  // }
+
+  const handleChange = event => {
+    const {value} = event.target
+    setInputLowercase(value.toLowerCase())
+  }
+
   // 🐨 add the onSubmit handler to the <form> below
 
   // 🐨 make sure to associate the label to the input.
   // to do so, set the value of 'htmlFor' prop of the label to the id of input
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div>
-        <label>Username:</label>
-        <input type="text" />
+        <label htmlFor="user">Username:</label>
+        <input
+          id="user"
+          type="text"
+          onChange={handleChange}
+          value={inputLowercase}
+        />
+        {/* <p role="alert" style={{color: 'red', fontStyle: 'italic'}}>
+          {errorMessage}
+        </p> */}
       </div>
+      {/* <button type="submit" disabled={Boolean(errorMessage)}> */}
       <button type="submit">Submit</button>
     </form>
   )
